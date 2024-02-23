54d7748f13fb96cdc1d02ebb1be16d162fd6b5ce
Vedant Palekar <75372283+hoopinwhoopin@users.noreply.github.com>
2024-02-23 21:32:25 +0530

    Delete task2 directory

--- task2/model2.js
+++
@@ -1,8 +0,0 @@
-const mongoose = require('mongoose');
-
-const companySchema = new mongoose.Schema({
-    name: {type : String, required : true},
-    holdings: { type : String, required : true },
-});
-
-module.exports = mongoose.model('companyData', companySchema);
